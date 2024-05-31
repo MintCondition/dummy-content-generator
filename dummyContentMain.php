40929ea27bc9ac8b98a0a41e04ae20cd9dcdf7bd
Brian Wood <brian+github@mintconditionstudios.com>
2024-05-31 18:12:51 -0400

    Initial commit

---
+++ dummyContentMain.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Stratifi Dummy Content Generator
+Description: A plugin to create dummy content for WordPress sites in Development
+Version: 0.0.1
+Author: Brian Wood (Stratifi Creative)
+*/
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+define('SITE_CONFIG_API_NAMESPACE', 'site-config/v1');
+define('SITE_CONFIG_API_ROUTE', '/data');
+define('SITE_CONFIG_API_TEST_ROUTE', '/test');
+
+require_once plugin_dir_path(__FILE__) . 'includes/update-checker.php';
+
+new GitHub_Updater(__FILE__, 'MintCondition', 'dummy-content-generator');
+
+add_action('rest_api_init', ['Site_Config_API', 'register_routes']);
